0dc2d1bffdea4e1fa3c119530949820cd1700da4
Alishba shekh <alishbashabbir890@gmail.com>
2025-03-15 01:48:44 -0700

    ..

---
+++ file_uploading/create.php
@@ -0,0 +1,86 @@
+<?php
+ 
+if(isset($_POST['submit'])){
+    $name=$_POST['stud-name'];
+    $email=$_POST['stud-email'];
+    $phone=$_POST['stud-phone'];
+    $image=$_FILES['stud-image']['name'];
+
+    $sql="INSERT INTO students (student name, student email, student phone, student image) VALUES()"
+
+
+
+} 
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>php crud</title>
+  </head>
+  <body>
+   <div class="container my-5">
+      <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                 <h4>php image uploading form</h4>
+                </div>
+                <div class="card-body">
+                 <form action="create.php" method="POST" enctype="multipart/form-data">
+                 <div class="form-group mt-3">
+                    <label for="">student name</label>
+                    <input type="text" name="stud-name" class="form-control" placeholder="enter name here">
+                 </div>
+                 <div class="form-group mt-3">
+                    <label for="">student email</label>
+                    <input type="email" name="stud-email" class="form-control" placeholder="enter email here">
+                 </div>
+                 <div class="form-group mt-3">
+                    <label for="">student phone</label>
+                    <input type="text" name="stud-phone" class="form-control" placeholder="enter phone no. here">
+                 </div>
+                 <div class="form-group mt-3">
+                    <label for="">student image</label>
+                    <input type="file" name="stud-image" class="form-control">
+                 </div>
+                <div class="form-group mt-4" >
+                    <button name="submit" class="btn btn-primary">submit</button>
+                </div>
+
+                 </form>
+                </div>
+            </div>
+        </div>
+      </div>
+   </div>
+
+  
+ 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+   
+  </body>
+</html>
